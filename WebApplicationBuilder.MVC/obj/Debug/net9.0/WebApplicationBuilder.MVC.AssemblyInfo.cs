6b230904ee948ad11252d9f9053e2db4d77c452b
suvoislam123 <suvoislam753@gmail.com>
2025-04-21 21:33:02 +0600

    swithcing operating system.

--- WebApplicationBuilder.MVC/obj/Debug/net9.0/WebApplicationBuilder.MVC.AssemblyInfo.cs
+++ WebApplicationBuilder.MVC/obj/Debug/net9.0/WebApplicationBuilder.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationBuilder.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4b0404908a75d7ad6fbba4ebd1c6f77f5839a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationBuilder.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationBuilder.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
